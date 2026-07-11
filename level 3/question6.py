e7e18613a30b1a4ec15d1a42cca716b04a4f98bc
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 15:07:58 +0530

    question6.py

---
+++ level 3/question6.py
@@ -0,0 +1,7 @@
+# find the smallest number in list.
+def smallest_num(my_list):
+    return min(my_list)
+
+my_list=[56,3,5,87,65,90]
+result=smallest_num(my_list)
+print(f"{my_list}\nSamllest num = {result}")
