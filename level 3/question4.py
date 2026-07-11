3e97ff9343683f78fdc0a474f1021bf9619bdd26
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 14:56:13 +0530

    question4.py

---
+++ level 3/question4.py
@@ -0,0 +1,13 @@
+# find the largest number in list without useing max().
+def largest_num(user_list):
+    largest = user_list[0]
+    for x in user_list:
+        if x > largest:
+            largest = x
+    return largest 
+
+user_list=[31,45,8,19,34,67,3,98,44]
+result=largest_num(user_list)
+print(f"{user_list}\nlargest num = {result}")
+      
+
