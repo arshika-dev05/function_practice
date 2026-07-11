ad79126e8abefc1a1839fea674f58154f88140f5
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 20:26:13 +0530

    question11.py

--- level 3/question11.py
+++ level 3/question11.py
@@ -1,3 +1,10 @@
 # function practice
 # reverse the list without using reverse()
+def reverse_list(list):
+    reverse=list[::-1]
+    return reverse
+
 my_list=[1,2,3,4,5,6,7,8]
+result=reverse_list(my_list)
+print("Reverse=",result)
+ 
