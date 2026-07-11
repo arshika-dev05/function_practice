6c328a93a654ba0e96b47e2ebb56b226687e6de3
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-11 15:18:41 +0530

    question7.py

---
+++ level 3/qusetion7.py
@@ -0,0 +1,13 @@
+# find smallest number in the list, but without useing min().
+def smallest_num(my_lsit):
+    smallest=my_lsit[0]
+    for i in my_list:
+        if i < smallest:
+            smallest = i
+    return smallest
+
+my_list=[25,65,34,78,21,20,45]
+result=smallest_num(my_list)
+print(f"{my_list}\nSmallest num = {result}")
+
+
