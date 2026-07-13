9a57981aa0c62fa01edf5ffd12362b4308b1af37
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-13 15:48:23 +0530

    question4.py

--- level 4/question4.py
+++ level 4/question4.py
@@ -3,12 +3,14 @@ def lowercase(text):
     word_store=" "
     for x in text:
         if x >='A' and x <='Z':
-            char_into_no=ord(x) - 32
+            char_into_no=ord(x) + 32
             no_into_char=chr(char_into_no)
             word_store += no_into_char
+        else:
+            word_store += x    
     return word_store        
 
 
-meassage=input("enter message")   
+meassage=input("enter message=")   
 result=lowercase(meassage)
 print(f"{meassage} conver in uppercase, ok \n{result}") 
